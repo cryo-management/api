90c4368bf24f950416c0c2567d541a5b1cba4dc9
André Luiz Mendonça <andreluzz@gmail.com>
2019-04-05 09:55:26 -0300

    New test suite

--- structure/schema/schema_test.go
+++ structure/schema/schema_test.go
@@ -4,11 +4,36 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCreate(t *testing.T) {
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including assertion methods.
+type SchemaTestSuite struct {
+	suite.Suite
+	VariableThatShouldStartAtFive int
+}
 
-	// assert equality
-	assert.Equal(t, 123, 123, "they should be equal")
+// Make sure that VariableThatShouldStartAtFive is set to five
+// before each test
+func (suite *SchemaTestSuite) SetupTest() {
+	suite.VariableThatShouldStartAtFive = 5
 
+	//db connect
+}
+
+// All methods that begin with "Test" are run as tests within a
+// suite.
+func (suite *SchemaTestSuite) Test001Example() {
+	assert.Equal(suite.T(), 5, suite.VariableThatShouldStartAtFive)
+}
+
+func (suite *SchemaTestSuite) Test002Example() {
+	assert.Equal(suite.T(), 5, suite.VariableThatShouldStartAtFive)
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestSchemaTestSuite(t *testing.T) {
+	suite.Run(t, new(SchemaTestSuite))
 }
