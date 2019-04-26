1a30e2ad89138ce48fed4fa8eafd51b834fc1ebc
André Luiz Mendonça <andreluzz@gmail.com>
2019-04-26 10:38:19 -0300

    Tree and currency

--- db.sql
+++ db.sql
@@ -1,3 +1,54 @@
+CREATE TABLE core_tree (
+  id CHARACTER VARYING DEFAULT uuid_generate_v4() NOT NULL,
+  code CHARACTER VARYING NOT NULL,
+  active BOOLEAN DEFAULT FALSE NOT NULL,
+  created_by CHARACTER VARYING NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_by CHARACTER VARYING NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE core_tree_levels (
+  id CHARACTER VARYING DEFAULT uuid_generate_v4() NOT NULL,
+  created_by CHARACTER VARYING NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_by CHARACTER VARYING NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE core_tree_units (
+  id CHARACTER VARYING DEFAULT uuid_generate_v4() NOT NULL,
+  parent_id CHARACTER VARYING,
+  code CHARACTER VARYING NOT NULL,
+  active BOOLEAN DEFAULT FALSE NOT NULL,
+  created_by CHARACTER VARYING NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_by CHARACTER VARYING NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE core_currencies (
+  id CHARACTER VARYING DEFAULT uuid_generate_v4() NOT NULL,
+  code CHARACTER VARYING NOT NULL,
+  active BOOLEAN DEFAULT FALSE NOT NULL,
+  created_by CHARACTER VARYING NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_by CHARACTER VARYING NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE core_currency_rates (
+  id CHARACTER VARYING DEFAULT uuid_generate_v4() NOT NULL,
+  currency_id CHARACTER VARYING NOT NULL,
+  value integer NOT NULL,
+  start_at TIMESTAMP NOT NULL,
+  end_at TIMESTAMP NOT NULL, 
+  created_by CHARACTER VARYING NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_by CHARACTER VARYING NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
 CREATE TABLE core_config_languages (
   id CHARACTER VARYING DEFAULT uuid_generate_v4() NOT NULL,
   code CHARACTER VARYING NOT NULL,
