ea6b9ce73d599af5634a4ce1e020d4fea16c240d
Bruno Piaui <brunopiaui@gmail.com>
2019-04-30 19:23:51 -0300

    Including comments to cors functions

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -6,11 +6,12 @@ import (
 	"github.com/go-chi/cors"
 )
 
+// Cors defines the security parameters to access to api
 func Cors() *cors.Cors {
 	cors := cors.New(cors.Options{
-		AllowOriginFunc:  AllowOriginFunc,
+		AllowOriginFunc:  allowOriginFunc,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "LanguageCode", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
@@ -19,7 +20,8 @@ func Cors() *cors.Cors {
 	return cors
 }
 
-func AllowOriginFunc(r *http.Request, origin string) bool {
+// allowOriginFunc defines a list of hosts authorized to access the api
+func allowOriginFunc(r *http.Request, origin string) bool {
 	if origin == "http://localhost:8080" {
 		return true
 	}
